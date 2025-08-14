51f0775a06cc02dbf4699a9421f4d7e0ea7ee498
PolamreddyChiranjeeviReddy <cjr45555@gmail.com>
2025-08-14 19:05:07 +0530

    Update StoreContext.tsx

--- admin-clg/src/context/StoreContext.tsx
+++ admin-clg/src/context/StoreContext.tsx
@@ -60,7 +60,7 @@ export const StoreContext = createContext<StoreContextType | null>(null);
 
 const StoreContextProvider = (props: StoreContextProviderProps) => {
 
-    const url = "adminclg-backend.vercel.app"
+    const url = "https://adminclg-backend.vercel.app"
     // const [food_list, setFoodList] = useState([]);
     // const [cartItems, setCartItems] = useState({});
     const [render,setRender] = useState(false);
